63dbcfe83a00a75296bbbd7301a60535f970e3ef
MarcoSkiadas <Skiadas2@hotmail.com>
2024-06-05 15:10:47 +0200

    Adding Inputs for User (Command-line interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,9 +15,9 @@ public class Main {
         productRepo.addProduct(product1);
         productRepo.addProduct(product2);
 
-        System.out.println(productRepo.getAllProducts());
-        System.out.println(productRepo.getProduct("1"));
-        System.out.println(productRepo.getProduct("3"));
+       // System.out.println(productRepo.getAllProducts());
+       // System.out.println(productRepo.getProduct("1"));
+       // System.out.println(productRepo.getProduct("3"));
 
         OrderListRepo orderListRepo = new OrderListRepo();
 
@@ -31,6 +32,45 @@ public class Main {
 
         shopService.placeNewOrder("1",newOrder);
 
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nEnter a Command: 'add_Product', 'remove_Product', 'list_Products', 'place_Order', 'list_orders', 'quit' \n");
+            String command = input.nextLine();
+
+            switch(command) {
+                case "add_Product":
+                    System.out.println("Enter a ProductID: ");
+                    String productID = input.nextLine();
+                    System.out.println("Enter a name: ");
+                    String productName = input.nextLine();
+                    System.out.println("Enter a price: ");
+                    int productPrice = input.nextInt();
+                    System.out.println("Enter a quantity: ");
+                    int productQty = input.nextInt();
+                    productRepo.addProduct(new Product(productID,productName, productPrice,productQty));
+                    System.out.println("Product "+productName+" has been added successfully");
+                    break;
+
+                case "remove_Product":
+                    System.out.println("Enter a ProductID: ");
+                    String removeProductID = input.nextLine();
+                    productRepo.removeProduct(productRepo.getProduct(removeProductID));
+
+                    break;
+                case "list_Products":
+                    break;
+                case "place_Order":
+                    break;
+                case "list_orders":
+                    break;
+                case "quit":
+                    System.out.println("Exiting the system.");
+                    input.close();
+                    break;
+                default:
+                    System.out.println("Unknown command.");
+            }
+        }
 
     }
 }
